fa423560776cee9a6b6c39ef1b6acb342eff03d1
roblabla <robinlambertz.dev@gmail.com>
2014-10-16 12:00:15 +0200

    Implement password lookup

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ var thunkify = require('thunkify');
 var question = require('co-prompt');
 var fs = require('co-fs');
 var redimpala = require('RedImpalaLib');
+var repl = require('repl');
+var crypto = require('crypto');
+var uuid = require('uuid');
 
 var password = question.password;
 var getFullBackup = thunkify(redimpala.getFullBackup);
 var sendToken = thunkify(redimpala.sendToken);
-
-var getToken = function *(email) {
-  yield sendToken(email);
-  return yield question("What is your token ? ");
-}
+var registerUKI = thunkify(redimpala.registerUKI);
+var getPassword = thunkify(redimpala.getPassword);
 
 co(function *() {
   var config = {};
@@ -22,8 +22,26 @@ co(function *() {
   opts.login = yield question("What is your email ? ");
   if (config[opts.login] && config[opts.login].uki)
     opts.uki = config[opts.login].uki;
-  else
-    opts.token = yield getToken(opts.login);
+  else {
+    // Generate UKI and save it in config.
+    if (!config[opts.login])
+      config[opts.login] = {};
+    var tmp = yield [question("What is your token ? "), sendToken(opts.login)];
+    var form = {
+      token: tmp[0],
+      login: opts.login,
+    };
+    var tmp2 = yield registerUKI(form);
+    var uki = tmp2[0];
+    console.log(tmp2[1]);
+    config[opts.login].uki = opts.uki = uki;
+    yield fs.writeFile('config.json', JSON.stringify(config));
+  }
   opts.password = yield password("What is your password ? ");
-  console.log((yield getFullBackup(opts)).toString());
+  var backupstr = yield getFullBackup(opts);
+  opts = null; // Attempt to get rid of the in-memory master password.
+  while (true) {
+    var pass = yield question("Which password do you want ? ");
+    console.log(yield getPassword(backupstr, pass));
+  }
 })();
